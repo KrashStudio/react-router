d07212ca09cdd527c110002cfc356b9475991119
Felix Yadomi <felix.yadomi@gmail.com>
2019-08-28 15:44:39 +0200

    Compat: rebuild

--- npm-scripts/postinstall.js
+++ npm-scripts/postinstall.js
@@ -1,11 +1,10 @@
-var execSync = require('child_process').execSync
-var stat = require('fs').stat
+var execSync = require("child_process").execSync;
+var stat = require("fs").stat;
 
 function exec(command) {
-  execSync(command, { stdio: [0, 1, 2] })
+  execSync(command, { stdio: [0, 1, 2] });
 }
 
-stat('lib', function (error, stat) {
-  if (error || !stat.isDirectory())
-    exec('npm run build')
-})
+stat("lib", function(error, stat) {
+  if (error || !stat.isDirectory()) exec("yarn build");
+});
